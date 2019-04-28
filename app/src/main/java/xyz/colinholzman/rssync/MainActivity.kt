d8e102134bf67ba6d9b71e04fb031ea8596fa757
Colin Holzman <colin.holzman@protonmail.com>
2019-04-28 16:25:26 -0400

    getting urls from webfinger and starting browser with authurl

--- app/src/main/java/xyz/colinholzman/rssync/MainActivity.kt
+++ app/src/main/java/xyz/colinholzman/rssync/MainActivity.kt
@@ -11,32 +11,40 @@ import java.io.IOException
 import java.net.URL
 import java.net.URLConnection
 import android.content.Intent
-import com.google.gson.Gson
+import android.util.Log
+import android.webkit.URLUtil
+import com.google.gson.*
+import com.google.gson.reflect.TypeToken
+import java.lang.RuntimeException
+import java.lang.reflect.Type
 
 class Link {
-    val rel: String? = null
+    val rel: String = "rel"
     val type: String? = null
     val href: String? = null
-    val titles: Map<String, Any>? = null
-    val properties: Map<String, Any?>? = null
+    val titles: Map<String, String>? = null
+    val properties: Map<String, String?>? = null
 }
 
 class JRD {
     val expires: String? = null
-    val subject: String? = null
+    val subject: String = "subject"
     val aliases: Array<String>? = null
     val properties: Map<String, Any?>? = null
     val links: Array<Link>? = null
 }
 
 fun getUri(input: String): Uri {
-    return Uri.parse(input)
+    return Uri.parse(URLUtil.guessUrl(input))
 }
 
 class MainActivity : AppCompatActivity() {
 
     companion object {
         var gson = Gson()
+//            GsonBuilder()
+//                    .registerTypeAdapter(Properties::class.java, PropertiesDeserializer())
+//                    .create()
         val client = OkHttpClient()
     }
 
@@ -70,12 +78,20 @@ class MainActivity : AppCompatActivity() {
                     override fun onResponse(call: Call, response: Response) {
                         runOnUiThread {
                             if (response.code() == 200) {
+
                                 resultText.text = response.body()?.string()
 
-                                val result = gson.fromJson(resultText.text.toString(), JRD().javaClass)
+                                val result = gson.fromJson<JRD>(resultText.text.toString(), object: TypeToken<JRD>(){}.type)
 
-//                                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse())
-//                                startActivity(browserIntent)
+                                val href = result.links?.get(0)?.href
+                                val authUrl = result.links?.get(0)?.properties?.get("http://tools.ietf.org/html/rfc6749#section-4.2")
+                                val version = result.links?.get(0)?.properties?.get("http://remotestorage.io/spec/version")
+
+                                val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(authUrl))
+                                startActivity(browserIntent)
+
+                                Log.i("result", result.toString())
+
 
                             } else {
                                 resultText.text = "failure: ${response.code()}"
