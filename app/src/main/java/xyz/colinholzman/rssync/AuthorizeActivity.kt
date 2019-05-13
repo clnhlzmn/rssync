b1d1ab13514d484e3c4fe7948b214089f0ef9133
Colin Holzman <colin.holzman@protonmail.com>
2019-05-13 12:51:38 -0400

    start reworking in the style of rssync-desktop, have authorization activity working

---
+++ app/src/main/java/xyz/colinholzman/rssync/AuthorizeActivity.kt
@@ -0,0 +1,87 @@
+package xyz.colinholzman.rssync
+
+import android.content.Context
+import android.os.Bundle
+import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log
+import android.webkit.WebResourceRequest
+import android.webkit.WebView
+import android.webkit.WebViewClient
+
+import kotlinx.android.synthetic.main.activity_authorize.*
+import xyz.colinholzman.remotestorage_kotlin.Authorization
+import xyz.colinholzman.remotestorage_kotlin.Discovery
+import xyz.colinholzman.rssync.MainActivity.Companion.id
+
+class AuthorizeActivity : AppCompatActivity() {
+
+    companion object {
+        const val USER = "USER"
+    }
+
+    private class AuthTokenWebViewClient(
+        val redirectUrl: String,
+        val listener: OnAuthorizationListener?
+    ): WebViewClient() {
+        interface OnAuthorizationListener {
+            fun onAuthorizationGranted(token: String)
+            fun onAuthorizationDenied(reason: String)
+        }
+        override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+            Log.i("AuthTokenWebViewClient", request.url.toString())
+            if (request.url.toString().startsWith(redirectUrl)) {
+                val token = request.url.fragment?.removePrefix("access_token=")
+                val error = request.url.fragment?.removePrefix("error=")
+                if (token != null && token != request.url.fragment) {
+                    listener?.onAuthorizationGranted(token)
+
+                    return true
+                } else if (error != null && error != request.url.fragment) {
+                    listener?.onAuthorizationDenied(error)
+                    return true
+                } else {
+                    listener?.onAuthorizationDenied("unknown error")
+                    return true
+                }
+            }
+            return false
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authorize)
+        val webView = findViewById<WebView>(R.id.webView)
+        val user = intent.getStringExtra(USER)
+        Discovery.lookup(
+            user,
+            { finish() },
+            {
+                runOnUiThread {
+                    val authUrl = Authorization.getAuthQuery(it)
+                    webView.webViewClient =
+                        AuthTokenWebViewClient(
+                            Authorization.redirectUrl,
+                            object: AuthTokenWebViewClient.OnAuthorizationListener {
+                                override fun onAuthorizationGranted(token: String) {
+                                    val prefs = getSharedPreferences("rssync", Context.MODE_PRIVATE)
+                                    val editor = prefs.edit()
+                                    editor.putString("token", token)
+                                    editor.apply()
+                                    Log.i("AuthorizeActivity", "authorized: $token")
+                                    finish()
+                                }
+                                override fun onAuthorizationDenied(reason: String) {
+                                    Log.i("AuthorizeActivity", "denied")
+                                    finish()
+                                }
+                            }
+                        )
+                    webView.loadUrl(authUrl)
+                }
+            }
+        )
+    }
+
+}
--- app/src/main/java/xyz/colinholzman/rssync/MainActivity.kt
+++ app/src/main/java/xyz/colinholzman/rssync/MainActivity.kt
@@ -1,9 +1,9 @@
 package xyz.colinholzman.rssync
 
 import android.content.Context
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.widget.Button
 import android.widget.EditText
 import android.util.Log
@@ -12,109 +12,33 @@ import xyz.colinholzman.remotestorage_kotlin.Discovery
 import xyz.colinholzman.remotestorage_kotlin.RemoteStorage
 
 
-class MainActivity : AppCompatActivity(),
-    AuthorizeFragment.OnAuthorizationListener,
-    ConnectedFragment.OnConnectedInteractionListener {
+class MainActivity : AppCompatActivity() {
 
     companion object {
         val id = "MainActivity"
     }
 
-    var actionButton: Button? = null
+    var authorizeButton: Button? = null
+    var startButton: Button? = null
+    var saveButton: Button? = null
 
     var href: String? = null
     var token: String? = null
-
-    private fun initState() {
-        val prefs = getSharedPreferences("rssync", Context.MODE_PRIVATE)
-        token = prefs.getString("token", null)
-        href = prefs.getString("href", null)
-
-        if (token != null && href != null) {
-            setStateConnected()
-        } else {
-            setStateDisconnected()
-        }
-    }
-
-    private fun setStateDisconnected() {
-
-        val inputText = findViewById<EditText>(R.id.editTextInput)
-
-        href = null
-        token = null
-
-        val prefs = getSharedPreferences("rssync", Context.MODE_PRIVATE).edit()
-        prefs.remove("token")
-        prefs.remove("href")
-        prefs.apply()
-
-        actionButton!!.text = "Connect"
-        actionButton!!.setOnClickListener {
-            Discovery.lookup(
-                inputText.text.toString(),
-                {
-                    e -> Log.e(id, e)
-                },
-                { jrd ->
-                    href = Authorization.getHref(jrd)
-                    val authFragment =
-                        AuthorizeFragment.newInstance(Authorization.getAuthQuery(jrd))
-                    val transaction = supportFragmentManager.beginTransaction()
-                    transaction.replace(R.id.frame, authFragment)
-                    transaction.commit()
-                }
-            )
-        }
-
-        val blankFragment = BlankFragment.newInstance()
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame, blankFragment)
-        transaction.commit()
-
-    }
-
-    private fun setStateConnected() {
-
-        val prefs = getSharedPreferences("rssync", Context.MODE_PRIVATE).edit()
-        prefs.putString("token", token)
-        prefs.putString("href", href.toString())
-        prefs.apply()
-
-        actionButton!!.text = "Disconnect"
-        actionButton!!.setOnClickListener {
-            setStateDisconnected()
-        }
-
-        val connectedFragment = ConnectedFragment.newInstance(token!!)
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.frame, connectedFragment)
-        transaction.commit()
-        connectedFragment.rs = RemoteStorage(href!!, token!!)
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        actionButton = findViewById(R.id.buttonLookup)
-
-        initState()
-    }
-
-    override fun onAuthorizationGranted(token: String) {
-        this.token = token
-        setStateConnected()
-    }
-
-    override fun onAuthorizationDenied(reason: String) {
-        Log.i(id, reason)
-        setStateDisconnected()
-    }
-
-    override fun onError(what: String) {
-        Log.i(id, what)
-        setStateDisconnected()
+        val rsUserField = findViewById<EditText>(R.id.editTextRsUser)
+        authorizeButton = findViewById(R.id.buttonAuthorize)
+        authorizeButton?.setOnClickListener {
+            val intent = Intent(this, AuthorizeActivity::class.java).apply {
+                putExtra(AuthorizeActivity.USER, rsUserField.text.toString())
+            }
+            startActivity(intent)
+        }
+        startButton = findViewById(R.id.buttonStart)
+        saveButton = findViewById(R.id.buttonSave)
     }
 
 }
