8acb3cf1181ed9de0fb03f4169f2360f2358ea36
Colin Holzman <colin.holzman@protonmail.com>
2019-05-14 08:39:15 -0400

    add android paho client/service, looking for fields that don't exist in java URI

--- app/src/main/java/xyz/colinholzman/rssync/MQTT.kt
+++ app/src/main/java/xyz/colinholzman/rssync/MQTT.kt
@@ -3,33 +3,51 @@ package xyz.colinholzman.rssync
 import android.content.Context
 import org.eclipse.paho.client.mqttv3.*
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
+import org.eclipse.paho.android.service.MqttAndroidClient
 
 class MQTT(val context: Context, val server: String, val port: String, val user: String?, val password: String?, val notify: () -> Unit) {
 
-    private val broker = "tcp://$server:$port"
+    private val broker = "tcp://$user:$password@$server:$port"
     private val clientId = context.getSharedPreferences("rssync", Context.MODE_PRIVATE).getString("client_id", null)
-    private val client = MqttClient(broker, clientId, MemoryPersistence())
+    private val client = MqttAndroidClient(context, broker, clientId)
+
+    private var connected = false
 
     fun connect() {
-        val connOpts = MqttConnectOptions()
-        connOpts.isCleanSession = true
-        connOpts.userName = user
-        connOpts.password = password?.toCharArray()
-        client.connect(connOpts)
+        if (!connected) {
+            val connOpts = MqttConnectOptions()
+            connOpts.isCleanSession = true
+            connOpts.userName = user
+            connOpts.password = password?.toCharArray()
 
-        client.subscribe("rssync/#") { topic, _ ->
-            if (!topic.endsWith(clientId!!)) {
-                notify()
-            }
+            client.connect(connOpts,
+                object : IMqttActionListener {
+                    override fun onSuccess(asyncActionToken: IMqttToken?) {
+                        connected = true
+                        client.subscribe("rssync/#", 2) { topic, message ->
+                            if (!topic.endsWith(clientId!!)) {
+                                notify()
+                            }
+                        }
+                    }
+                    override fun onFailure(asyncActionToken: IMqttToken?, exception: Throwable?) {
+                        connected = false
+                    }
+                }
+            )
         }
     }
 
     fun publish() {
-        client.publish("rssync/$clientId", MqttMessage())
+        if (connected)
+            client.publish("rssync/$clientId", MqttMessage())
     }
 
     fun disconnect() {
-        client.disconnect()
+        if (connected) {
+            client.disconnect()
+            connected = false
+        }
     }
 
 }
